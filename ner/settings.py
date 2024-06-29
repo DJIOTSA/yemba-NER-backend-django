8c53209ace179924aae7d32d8a22a9435b142578
mhulo <87663526+DJIOTSA@users.noreply.github.com>
2024-06-29 16:58:29 +0100

    Update settings.py

--- ner/settings.py
+++ ner/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ukb7w55gt@vr0#do)65rgxg7^h*_pea!o^yv53j-xw%txlfv8i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
