2a902cf284a4b3836fbc710025560aebe695cc27
DJIOTSA CHRISTIAN (el Mhulo) <djiotsadjouakechristiandaryn@gmail.com>
2024-07-23 07:05:18 +0100

    update settings.py

--- app/views.py
+++ app/views.py
@@ -47,3 +47,89 @@ class HistoryCreateView(generics.CreateAPIView):
         accuracy = serializer.validated_data.get('accuracy')
         return serializer.save(user=user)
     
+
+
+
+def format_text_with_entities(input_text, entities):
+    """ Format input text into html format using defined named entities"""
+    import html
+    entity_descriptions = {
+        'GPE': 'geopolitical location',
+        'TIME': 'time',
+        'QUAN': 'quantity',
+        'GEO': 'geographic location',
+        'PER': 'person'
+    }
+    # Escape HTML special characters in the input text
+    input_text = html.escape(input_text)
+
+    # Sort entities by their start position
+    entities.sort(key=lambda x: x['start'])
+
+    # Merge sub-words into complete words or phrases and group them by the first entity
+    merged_entities = []
+    current_entity = None
+
+    for entity in entities:
+        if entity['word'].startswith("##"):
+            if current_entity is not None:
+                current_entity['word'] += entity['word'][2:]
+                current_entity['end'] = entity['end']
+        else:
+            if current_entity is not None:
+                merged_entities.append(current_entity)
+            current_entity = {
+                'entity': entity['entity'],
+                'word': entity['word'],
+                'start': entity['start'],
+                'end': entity['end']
+            }
+
+    if current_entity is not None:
+        merged_entities.append(current_entity)
+
+    # Group words with the same entity type together
+    grouped_entities = []
+    current_group = None
+
+    for entity in merged_entities:
+        if current_group is None:
+            current_group = entity
+        elif entity['entity'].startswith('I-') and current_group['entity'].startswith('B-'):
+            current_group['word'] += ' ' + entity['word']
+            current_group['end'] = entity['end']
+        else:
+            grouped_entities.append(current_group)
+            current_group = entity
+
+    if current_group is not None:
+        grouped_entities.append(current_group)
+
+    # Initialize variables
+    formatted_text = ""
+    last_index = 0
+
+    # Iterate through each grouped entity and format the text
+    for entity in grouped_entities:
+        start = entity['start']
+        end = entity['end']
+        entity_text = input_text[start:end]
+        # Use the first entity type for the entire group
+        entity_type = entity['entity'].split('-')[1]
+        entity_description = entity_descriptions.get(
+            entity_type, entity_type)  # Get the description
+
+        # Append the text before the entity
+        formatted_text += input_text[last_index:start]
+        # Format the entity text
+        formatted_text += f'<span class="entity {entity_type}">{entity_text} <sub>({entity_description})</sub></span>'
+        # Update the last index
+        last_index = end
+
+    # Append the remaining text after the last entity
+    formatted_text += input_text[last_index:]
+
+    return formatted_text
+
+
+    
